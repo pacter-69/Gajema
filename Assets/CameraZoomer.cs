de2cf699f9871e1406e76ddce026866c7306963d
ElBuenTocino <paumm8@gmail.com>
2025-07-04 11:48:59 +0200

    To Zoom shit

---
+++ Assets/CameraZoomer.cs
@@ -0,0 +1,30 @@
+using System.Drawing;
+using UnityEditor.Build.Content;
+using UnityEngine;
+
+public class CameraZoomer : MonoBehaviour
+{
+    public GameObject toZoom;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Zoom();
+    }
+
+    void Zoom()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            this.gameObject.transform.position = new Vector3(toZoom.transform.position.x, toZoom.transform.position.y, -10);
+            gameObject.GetComponent<Camera>().orthographicSize = CalculateSize();
+        }
+    }
+
+    float CalculateSize()
+    {
+        float size;
+        size = toZoom.transform.localScale.y / 2;
+        return size;
+    }
+}
