9df32aee37219d8f5d675fb42c325413e5a6646d
pacter-69 <aldomcorb@gmail.com>
2025-07-04 20:50:08 +0200

    hecho bueno ahora está más
    más está ahora hecho bueno

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -2,6 +2,7 @@ using System;
 using System.Xml;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CameraBehaviour : MonoBehaviour
 {
@@ -10,9 +11,11 @@ public class CameraBehaviour : MonoBehaviour
     private GameObject cameraTarget;
     public GameObject camera1, camera2, camera3;
     public float alpha;
-    private GameObject activeCamera;
+    public float zoomedOutSize, zoomedInSize;
+    private Vector3 camera1Origin, camera2Origin, camera3Origin;
+    public GameObject activeCamera;
     private bool isZooming, isZoomingOut, playerCanMove;
-    private float timer;
+    private float timer1, timer2;
     public enum EnumCamera
     {
         Playing,
@@ -24,37 +27,39 @@ public class CameraBehaviour : MonoBehaviour
     public EnumCamera enumCamera;
     void Start()
     {
-        activeCamera = camera3;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Camera.SetupCurrent(activeCamera.GetComponent<Camera>());
 
         if(activeCamera == camera1)
         {
+            activeCamera.GetComponent<Camera>().depth = 1;
             cameraTarget = camera1Target;
             activeCamera.tag = "MainCamera";
 
-            camera2.tag = "Untagged";
-            camera3.tag = "Untagged";
+            camera2.GetComponent<Camera>().depth = 0;
+            camera3.GetComponent<Camera>().depth = -1;
         }
         else if (activeCamera == camera2)
         {
+            activeCamera.GetComponent<Camera>().depth = 1;
             cameraTarget = camera2Target;
             activeCamera.tag = "MainCamera";
 
-            camera3.tag = "Untagged";
-            camera1.tag = "Untagged";
+            camera3.GetComponent<Camera>().depth = 0;
+            camera1.GetComponent<Camera>().depth = -1;
         }
         else if (activeCamera == camera3)
         {
+            activeCamera.GetComponent<Camera>().depth = 1;
             cameraTarget = camera3Target;
             activeCamera.tag = "MainCamera";
 
-            camera2.tag = "Untagged";
-            camera1.tag = "Untagged";
+            camera2.GetComponent<Camera>().depth = 0;
+            camera1.GetComponent<Camera>().depth = -1;
         }
 
         switch (enumCamera)
@@ -77,6 +82,8 @@ public class CameraBehaviour : MonoBehaviour
                     {
                         activeCamera = camera3;
                     }
+
+                    enumCamera = EnumCamera.Playing;
                 }
                 break;
             case EnumCamera.Zoomout1:
@@ -118,29 +125,33 @@ public class CameraBehaviour : MonoBehaviour
 
     public void Zoom()
     {
-        timer += Time.deltaTime;
-        activeCamera.transform.position = Vector3.Lerp(activeCamera.transform.position, cameraTarget.transform.position, alpha * Time.deltaTime);
+        isZooming = true;
+        timer1 += Time.deltaTime;
+        activeCamera.transform.position = Vector3.Lerp(activeCamera.transform.position, cameraTarget.transform.position, (alpha-1.25f) * Time.deltaTime);
         /*if (isZooming &&  (cameraTarget.transform.position - activeCamera.transform.position).magnitude < 0.1)
         {
             enumCamera = EnumCamera.Zoom2;
         }*/
-        if(timer > 1.5)
+        if(timer1 > 0.8)
         {
             enumCamera = EnumCamera.Zoom2;
+            timer1 = 0;
         }
     }
 
     public void Zoom2()
     {
-        timer += Time.deltaTime;
-        activeCamera.GetComponent<Camera>().orthographicSize = Mathf.Lerp(activeCamera.GetComponent<Camera>().orthographicSize, 1.1f, alpha * Time.deltaTime);
+        timer2 += Time.deltaTime;
+        activeCamera.GetComponent<Camera>().orthographicSize = Mathf.Lerp(activeCamera.GetComponent<Camera>().orthographicSize, cameraTarget.transform.localScale.x/2, alpha * Time.deltaTime);
 /*        if(1.1f - activeCamera.GetComponent<Camera>().orthographicSize < 0.1)
         {
             isZooming = false;
         }*/
-        if (timer > 1.5)
+        if (timer2 > 1.35)
         {
             enumCamera = EnumCamera.Zoom2;
+            timer2 = 0;
+            isZooming = false;
         }
     }
 
